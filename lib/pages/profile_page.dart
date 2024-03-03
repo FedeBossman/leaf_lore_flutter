649c3ab7f1844ec10f93cd7657f35922b0e6f82b
Fede Gutierrez <f.gfaraoni@gmail.com>
2024-03-03 00:24:45 -0500

    Profile page with sign out

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProfilePage extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Profile Page'),
-          ],
+Widget build(BuildContext context) {
+    final User? user = _auth.currentUser;
+    final String name = user?.displayName ?? 'No Name Available';
+    final String email = user?.email ?? 'No Email Available';
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: ListView(
+            children: [
+              SizedBox(height: 20),
+              ListTile(
+                leading: Icon(Icons.account_circle, size: 36, color: Colors.green),
+                title: Text(name, style: TextStyle(fontSize: 18)),
+                subtitle: Text('Name', style: TextStyle(color: Colors.black.withOpacity(0.6))),
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.email, size: 36, color: Colors.green),
+                title: Text(email, style: TextStyle(fontSize: 18)),
+                subtitle: Text('Email', style: TextStyle(color: Colors.black.withOpacity(0.6))),
+              ),
+              Divider(),
+            ],
+          ),
         ),
-      );
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+          child: ElevatedButton(
+            onPressed: () async {
+              await _auth.signOut();
+              Navigator.of(context).popUntil((route) => route.isFirst);
+            },
+            child: Text('Sign Out', style: TextStyle(fontSize: 16)),
+            style: ElevatedButton.styleFrom(
+              // primary: Colors.red, // Button color
+              // onPrimary: Colors.white, // Text color
+              minimumSize: Size(double.infinity, 50), // Button size
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
